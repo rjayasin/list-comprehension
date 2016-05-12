f2b953d99f54a4a326756ca909c868301e5c9b51
rjayasin <rjayasin@users.noreply.github.com>
2016-05-11 23:47:21 -0700

    Added some timing code. My version takes twice as long as the list difference!

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,11 +1,19 @@
 import math
+import time
 
-#compute primes using set difference
+#compute primes using list difference
 #from http://www.secnetix.de/olli/Python/list_comprehensions.hawk
+t0 = time.clock()
 noprimes = [j for i in range(2, 8) for j in range(i*2, 50, i)]
 difference = [x for x in range(2, 50) if x not in noprimes]
-print(difference)
+# print(difference)
+t1 = time.clock()
+print("Time for list difference:", t1 - t0)
 
 #my own version, a little more complicated
+t2 = time.clock()
+# noprimes = [j for i in range(2, 8) for j in range(i*2, 50, i)]
 primes = [x for x in range(1, 51) if not any([y for y in range(2, int(math.sqrt(x) + 1)) if x % y == 0])]
-print(primes)
+# print(primes)
+t3 = time.clock()
+print("Time for my comprehension:", t3 - t2)
